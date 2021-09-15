acc481b6481337ad525d587a4f311dd5c0804fe8
DELL <DELL@DESKTOP-IHKGJNI>
2021-09-15 18:37:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Minipro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i, fact = 1, n;
+            Console.Write("Enter any number");
+            n = int.Parse(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                fact = fact * i;
+            }
+            Console.Write("factorial of   " + n + "  is  " + fact);
+
+        }
+    }
+}
